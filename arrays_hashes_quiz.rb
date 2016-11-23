f35c5640ec3ed0054e3aaf60b435855c7e85b3a8
Graeme Cameron <gcameron4981@gmail.com>
2016-11-23 16:03:53 +0000

    part A done

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -4,24 +4,41 @@ lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'P
 
 # 1. Work out how many stops there are in the array
 
-puts lines.length()
-
+#puts lines.length()
 
 # 2. Return 'Edinburgh Park' from the array
 
+#puts lines[1]
+
 # 3. How many ways can we return 'Princes Street' from the array?
+
+#puts lines [4]
+#puts lines [-1]
 
 # 4. Work out the index position of 'Haymarket'
 
+#puts lines.index("Haymarket")
+
 # 5. Add 'Airport' to the start of the array
+
+#lines.unshift("Airport")
 
 # 6. Add 'York Place' to the end of the array
 
+#lines.push("York Place")
+
 # 7. Remove 'Edinburgh Park' from the array using it's name
+
+#lines.delete("Edinburgh Park")
 
 # 8. Delete 'Edinburgh Park' from the array by index
 
+#lines.delete_at(1)
+
 # 9. Reverse the positions of the stops in the array
+
+lines.reverse!()
+puts lines
 
 ### B. Given the following data structure:
 

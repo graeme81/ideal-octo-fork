43696f6042eab5a60e89b659eec2d29bf03cbadc
Graeme Cameron <gcameron4981@gmail.com>
2016-11-23 16:12:26 +0000

    Part b finished

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -37,8 +37,8 @@ lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'P
 
 # 9. Reverse the positions of the stops in the array
 
-lines.reverse!()
-puts lines
+#lines.reverse!()
+#puts lines
 
 ### B. Given the following data structure:
 
@@ -46,13 +46,26 @@ my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # 1. How would you return the string `"One"`?
 
+puts my_hash[1]
+
 # 2. How would you return the string `"Two"`?
+
+puts my_hash[:two]
 
 # 3. How would you return the number `2`?
 
-# 4. How would you add `{3 => "Three"}` to the hash?
+puts my_hash["two"]
+
+# 4. How would you add `{3 => "Three"}` to the hash? 
+
+my_hash[3] = "Three"
+puts my_hash
 
 # 5. How would you add `{:four => 4}` to the hash?
+
+my_hash[:four] = 4
+puts my_hash
+
 
 ### C. Given the following data structure:
 
